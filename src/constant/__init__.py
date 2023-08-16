8f593c86d042e574a1d8e3b46eb5550b06d506fe
adarshmarvel22 <adarshmarvel22@gmail.com>
2023-08-16 21:01:38 +0530

    src

--- src/constant/__init__.py
+++
@@ -1,3 +0,0 @@
-AWS_S3_BUCKET_NAME="phishing-domain-prediction"
-DATABASE_NAME="ineuron"
-COLLECTION_NAME="phishingdomain"
--- src/pipeline/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
- 
-# import sys
-# from src.exception import CustomException
-# from src.logger import logging
-# import pandas as pd
